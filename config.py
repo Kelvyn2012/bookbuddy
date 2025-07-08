9c416445a8f85760411b2490706aad3f0466daa4
kelvyn2012 <kudufo@gmail.com>
2025-07-08 16:22:29 +0100

    updated my config files

--- config.py
+++ config.py
@@ -1,12 +1,11 @@
-# Database configuration
 import os
 from dotenv import load_dotenv
 
 load_dotenv()  # Loads variables from .env into environment
 
 MYSQL_CONFIG = {
-    "host": os.getenv("localhost"),
-    "user": os.getenv("root"),
-    "password": os.getenv("Jenifer2012%"),
-    "database": os.getenv("bookbuddyDB"),
+    "host": os.getenv("DB_HOST"),
+    "user": os.getenv("DB_USER"),
+    "password": os.getenv("DB_PASSWORD"),
+    "database": os.getenv("DB_NAME"),
 }
